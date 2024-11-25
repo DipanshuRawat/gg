5a7d364bd3c266318e2b4aa6904c5e55a3bac9ab
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-25 10:54:46 +0530

    Update and rename hlo.groovy to Bar.groovy

---
+++ src/org/foo/Bar.groovy
@@ -0,0 +1,11 @@
+package org.foo
+
+class Bar {
+    static String greet(String name) {
+        return "Hello, ${name}!"
+    }
+
+    static int add(int a, int b) {
+        return a + b
+    }
+}
