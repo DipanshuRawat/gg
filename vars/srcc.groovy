cbeeffab29aa4a1f91e816a93ddc92574591c8a9
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-25 10:55:47 +0530

    Update srcc.groovy

--- vars/srcc.groovy
+++ vars/srcc.groovy
@@ -1,3 +1,6 @@
-def call() {
-    echo "Hello"
+import org.foo.Bar
+
+def call(String name) {
+    echo Bar.greet(name)  // Use the class method
+    echo "Sum: ${Bar.add(5, 7)}"  // Use another class method
 }
