1cc6b7a43a5fd85de799c814a1660ea9815b61fb
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-25 10:42:12 +0530

    Update d.groovy

--- vars/d.groovy
+++ vars/d.groovy
@@ -1,4 +1,6 @@
-def call() {
-    echo "Dipanshu"
+def call(String name) {
+    def json = libraryResource('org/foo/bar.json')  // Load resource file
+    def data = new groovy.json.JsonSlurper().parseText(json)  // Parse JSON
+    
+    echo "${data.greeting}, ${name}!"  // Outputs: Hello, <name>!
 }
-
