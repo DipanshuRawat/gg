988ca520fc131fd9d510a402ea52097aeecccdd5
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-27 01:19:01 +0530

    Update installMongo.groovy

--- vars/installMongo.groovy
+++ vars/installMongo.groovy
@@ -1,8 +1,12 @@
+// Inside your shared library (my-Library)
 def call(Map params = [:]) {
-    def playbook = params.get('playbook', 'resources/org/foo/installmongo.yml')
-    def inventory = params.get('inventory', 'resources/org/foo/hosts')
+    def playbook = params.get('playbook', 'resources/org/foo/installmongo.yml')  // Default if not passed
+    def inventory = params.get('inventory', 'resources/org/foo/hosts')  // Default if not passed
+
+    // Fetch SSH key from Jenkins credentials
+    def sshKey = params.get('sshKey', credentials('3d11f520-4c0f-4e15-acd0-404b4398863d'))  // Replace with your credential ID
 
     sh """
-    ansible-playbook -i ${inventory} ${playbook}
+        ansible-playbook -i ${inventory} ${playbook} --private-key ${sshKey}
     """
 }
