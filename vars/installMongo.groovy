f8872114afc01ad803dde809316b1c3ec62ff905
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-27 00:43:31 +0530

    Update installMongo.groovy

--- vars/installMongo.groovy
+++ vars/installMongo.groovy
@@ -1,17 +1,8 @@
-def call(Map args = [:]) {
-    def configFile = args.get('configFile', 'config/mongoDeployConfig.yml')
+def call(Map params = [:]) {
+    def playbook = params.get('playbook', 'resources/org/foo/installmongo.yml')
+    def inventory = params.get('inventory', 'resources/org/foo/hosts')
 
-    stage('User Approval') {
-        input message: "Approve MongoDB Deployment?"
-    }
-
-    stage('Execute Ansible Playbook') {
-        script {
-            def config = readYaml(file: configFile)
-            ansiblePlaybook(
-                playbook: "${libraryResource('org/foo/installmongo.yml')}",
-                inventory: config.inventoryFile
-            )
-        }
-    }
+    sh """
+    ansible-playbook -i ${inventory} ${playbook}
+    """
 }
