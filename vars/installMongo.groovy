2aaef6b9c5ec993edb9e6f36e3b6d1bd2d1baeb0
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-28 11:49:32 +0530

    Update installMongo.groovy

--- vars/installMongo.groovy
+++ vars/installMongo.groovy
@@ -4,9 +4,15 @@ def call(Map params = [:]) {
     def inventory = params.get('inventory', 'resources/org/foo/hosts')          // Default inventory path
     def credentialsId = params.get('sshKey', '1dfb88aa-b117-42bf-a70a-12e98a81d7d1') // Credential ID
 
+    // Debugging output (remove in production)
+    echo "Playbook: ${playbook}"
+    echo "Inventory: ${inventory}"
+    echo "Credentials ID: ${credentialsId}"
+
     // Use Jenkins credentials securely
     withCredentials([sshUserPrivateKey(credentialsId: credentialsId)]) {
         sh """
+            echo "Running Ansible playbook with inventory: ${inventory} and playbook: ${playbook}"
             ansible-playbook -i ${inventory} ${playbook}
         """
     }
