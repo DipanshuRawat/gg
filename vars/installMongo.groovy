ec2347167eb20e01ec01fa563204256d37de7f4d
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-29 12:06:11 +0530

    Update installMongo.groovy

--- vars/installMongo.groovy
+++ vars/installMongo.groovy
@@ -1,19 +1,16 @@
-// Inside your shared library (vars/installMongo.groovy)
-def call(Map params = [:]) {
-    def playbook = params.get('playbook', 'resources/org/foo/installmongo.yml')  // Default playbook path
-    def inventory = params.get('inventory', 'resources/org/foo/hosts')          // Default inventory path
-    def credentialsId = params.get('sshKey', '1dfb88aa-b117-42bf-a70a-12e98a81d7d1') // Credential ID
-
-    // Debugging output (remove in production)
-    echo "Playbook: ${playbook}"
-    echo "Inventory: ${inventory}"
-    echo "Credentials ID: ${credentialsId}"
-
-    // Use Jenkins credentials securely
-    withCredentials([sshUserPrivateKey(credentialsId: credentialsId)]) {
-        sh """
-            echo "Running Ansible playbook with inventory: ${inventory} and playbook: ${playbook}"
-            ansible-playbook -i ${inventory} ${playbook}
-        """
+def call(){
+    pipeline {
+        agent any
+        environment {
+            ANSIBLE_SSH_ARGS = '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+        }
+        stages {
+            }
+            stage('Install MongoDB') {
+                steps {
+                    ansiblePlaybook credentialsId: '1dfb88aa-b117-42bf-a70a-12e98a81d7d1', disableHostKeyChecking: true, installation: 'ansible', inventory: 'resources/org/foo/aws_ec2.yml', playbook: 'resources/org/foo/installmongo.yml', vaultTmpPath: ''
+                }
+            }
+        }
     }
 }
