ccb5acf70f7b39297af49cc48e44ea0e576e4d1a
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2024-11-29 12:28:20 +0530

    Update installMongo.groovy

--- vars/installMongo.groovy
+++ vars/installMongo.groovy
@@ -1,14 +1,20 @@
-def call(){
+def call() {
     pipeline {
         agent any
         environment {
             ANSIBLE_SSH_ARGS = '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
         }
         stages {
-            }
             stage('Install MongoDB') {
                 steps {
-                    ansiblePlaybook credentialsId: '1dfb88aa-b117-42bf-a70a-12e98a81d7d1', disableHostKeyChecking: true, installation: 'ansible', inventory: 'workspace/resources/org/foo/aws_ec2.yml', playbook: 'workspace/resources/org/foo/installmongo.yml', vaultTmpPath: ''
+                    ansiblePlaybook(
+                        credentialsId: '1dfb88aa-b117-42bf-a70a-12e98a81d7d1',
+                        disableHostKeyChecking: true,
+                        installation: 'ansible',
+                        inventory: 'workspace/resources/org/foo/aws_ec2.yml',
+                        playbook: 'workspace/resources/org/foo/installmongo.yml',
+                        vaultTmpPath: ''
+                    )
                 }
             }
         }
